edc2d8e8af6a3f9144b181654ccb3b7750e15756
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-06-26 16:53:12 -0400

    Add timers to complete one full rotation of the spinner animation

--- spinner1.js
+++ spinner1.js
@@ -14,7 +14,25 @@ setTimeout(() => {
 
 setTimeout(() => {
   // Need to escape the backslash since it's a special character.
-  process.stdout.write('\r\\   '); 
+  process.stdout.write('\r\\   ');
 }, 700);
 
-// ... fill in the rest yourself ...
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 900);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 1100);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 1300);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   ');
+}, 1500);
+
+setTimeout(() => {
+  process.stdout.write('\r|   \n');
+}, 1700);
