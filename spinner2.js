7ef01904be293a5cf29c47cb76c461833d0c9263
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-06-27 11:50:38 -0400

    Take command line argument for number of rotations

--- spinner2.js
+++ spinner2.js
@@ -1,9 +1,39 @@
-const symbols = "|/-\\|/-\\|/-\\|/-\\|/-\\|/-\\|/-\\|/-\\|/-\\|/-\\|/-\\|/-\\|/-\\|/-\\|/-\\|/-\\|/-\\|/-\\|/-\\|/-\\\n";
+// 20 rotations by default
+const symbols = "|/-\\|/-\\|/-\\|/-\\|/-\\|/-\\|/-\\|/-\\|/-\\|/-\\|/-\\|/-\\|/-\\|/-\\|/-\\|/-\\|/-\\|/-\\|/-\\|/-\\|\n";
 let time = 100;
 
-for (let i = 0; i < symbols.length; i++) {
-  setTimeout(() => {
-    process.stdout.write('\r' + symbols[i] + "   ");
-  }, time);
-  time += 200;
-}
+// user can specify desired number of rotations via command line argument:
+let rotations = process.argv[2];
+let userSpecified = "";
+
+const spinner = (rotations) => {
+  if (rotations) {
+    for (let i = 0; i < rotations; i++) {
+      userSpecified += "|/-\\";
+    }
+    userSpecified += "|";
+
+    for (let i = 0; i < userSpecified.length; i++) {
+      setTimeout(() => {
+        process.stdout.write("\r" + userSpecified[i] + "   ");
+      }, time);
+      time += 200;
+    }
+
+    setTimeout(() => {
+      process.stdout.write("\n");
+    }, time);
+    time += 200;
+
+  } else {
+    // default behaviour:
+    for (let i = 0; i < symbols.length; i++) {
+      setTimeout(() => {
+        process.stdout.write("\r" + symbols[i] + "   ");
+      }, time);
+      time += 200;
+    }
+  }
+};
+
+spinner(rotations);
